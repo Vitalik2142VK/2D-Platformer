e9bc6bc32c02583797b1740e5552530bf935372e
Vitalik2142VK <120681864+Vitalik2142VK@users.noreply.github.com>
2024-03-20 15:51:22 +0300

    Fixed bugs

--- Assets/Scripts/Health/HealthBar.cs
+++ Assets/Scripts/Health/HealthBar.cs
@@ -1,44 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour
+public abstract class HealthBar : MonoBehaviour
 {
     private const float RotateY = 180.0f;
 
     [SerializeField] private Health _health;
-    [SerializeField, Min(20.0f)] private float _speedChangeHealth = 10.0f;
-    [SerializeField] private bool _isSmoothnessEnabled = true;
 
-    private Slider _healthBar;
-    private float _targetHealth;
+    protected Slider HealthScale;
 
     private Camera _camera;
 
-    private void Awake()
+    protected Health Health => _health;
+
+    protected void Awake()
     {
-        _healthBar = GetComponent<Slider>();
+        HealthScale = GetComponent<Slider>();
         _camera = Camera.main;
     }
 
     private void OnEnable()
     {
         _health.HealthChange += UpdateValueHealthBar;
-    }
-
-    private void Start()
-    {
-        _targetHealth = _health.MaxCountHealth;
-
-        _healthBar.maxValue = _targetHealth;
-        _healthBar.value = _targetHealth;
-    }
-
-    private void Update()
-    {
-        if (_isSmoothnessEnabled)
-        {
-            _healthBar.value = Mathf.MoveTowards(_healthBar.value, _targetHealth, _speedChangeHealth * Time.deltaTime);
-        }
     }
 
     private void LateUpdate()
@@ -52,13 +35,5 @@ public class HealthBar : MonoBehaviour
         _health.HealthChange -= UpdateValueHealthBar;
     }
 
-    private void UpdateValueHealthBar(float health)
-    {
-        if (_isSmoothnessEnabled == false)
-        {
-            _healthBar.value = health;
-        }
-
-        _targetHealth = health;
-    }
+    protected abstract void UpdateValueHealthBar(float health);
 }
